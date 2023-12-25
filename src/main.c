c322074af9213092bf142d28071683835303c526
augustodsgv <augustodsgv@gmail.com>
2023-12-25 17:53:57 -0300

    update readme

--- src/main.c
+++ src/main.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 
 int main(int argc, const char * argv[]){
-    // field * campo = load_field_save("saved.txt");
-
-    // /*
     int fieldSize = 4;
     if(argc > 1) fieldSize = (int)strtol(argv[1], NULL, 10);
     field * campo = build_field(fieldSize);
